e2a46add1607c4dac8d672c9ab7c7b565788194e
tanvir07-cell <tr9836859@gmail.com>
2024-05-11 22:56:40 -0700

    Chore:for the force install ai model package

---
+++ src/dist/assets/Modal.19376bc7.js
@@ -0,0 +1,1 @@
+import{r,a as n,j as a}from"./index.79a3eeaa.js";const s=({children:o})=>{const e=r.exports.useRef(null);return e.current||(e.current=document.createElement("div")),r.exports.useEffect(()=>{const t=document.getElementById("modal");return t.appendChild(e.current),()=>t.removeChild(e.current)},[]),n.exports.createPortal(a("div",{children:o}),e.current)};export{s as default};
